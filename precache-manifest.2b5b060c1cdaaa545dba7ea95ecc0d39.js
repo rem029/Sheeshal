ecb659396d590040cfdba5ee8a7cbdf2d3cdc26d
rem029 <elawrenceponce@gmail.com>
2020-08-26 15:40:17 +0300

    Updates

---
+++ precache-manifest.2b5b060c1cdaaa545dba7ea95ecc0d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a8ec2a185d83b26582f175754de2be",
+    "url": "/Sheeshal/index.html"
+  },
+  {
+    "revision": "12f89073fcec0e8fb7f3",
+    "url": "/Sheeshal/static/css/main.e6ddec68.chunk.css"
+  },
+  {
+    "revision": "a41b9c6b6879e7a6ff12",
+    "url": "/Sheeshal/static/js/2.8d92ebc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sheeshal/static/js/2.8d92ebc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f89073fcec0e8fb7f3",
+    "url": "/Sheeshal/static/js/main.5af8b2be.chunk.js"
+  },
+  {
+    "revision": "57ecd6bfafb582fdf733",
+    "url": "/Sheeshal/static/js/runtime-main.d1fb8054.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sheeshal/precache-manifest.05061db326c860645f42ec986118037e.js"
+  "/Sheeshal/precache-manifest.2b5b060c1cdaaa545dba7ea95ecc0d39.js"
 );
 
 self.addEventListener('message', (event) => {
