34f46729e4c6cb29b88a9fb14c68856bfc1ec071
rem029 <elawrenceponce@gmail.com>
2020-08-26 08:31:46 +0300

    Updates

---
+++ precache-manifest.86ff65f4d522f9657b513fbce054fb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6f227b078b72e7bfb331089ad91753",
+    "url": "/Sheeshal/index.html"
+  },
+  {
+    "revision": "8e492522f9edb4315f1b",
+    "url": "/Sheeshal/static/css/main.41de52cd.chunk.css"
+  },
+  {
+    "revision": "d9e7117d464babe417f2",
+    "url": "/Sheeshal/static/js/2.8c0308e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sheeshal/static/js/2.8c0308e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e492522f9edb4315f1b",
+    "url": "/Sheeshal/static/js/main.975a8ad0.chunk.js"
+  },
+  {
+    "revision": "57ecd6bfafb582fdf733",
+    "url": "/Sheeshal/static/js/runtime-main.d1fb8054.js"
+  }
+]);
