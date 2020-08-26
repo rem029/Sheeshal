6c1a0f959b24746a67ab4002379e761508afa91e
rem029 <elawrenceponce@gmail.com>
2020-08-26 15:26:09 +0300

    Updates

--- precache-manifest.05061db326c860645f42ec986118037e.js
+++ precache-manifest.05061db326c860645f42ec986118037e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb6f227b078b72e7bfb331089ad91753",
+    "revision": "66b0967793be35e92e20ff699bc250e1",
     "url": "/Sheeshal/index.html"
   },
   {
-    "revision": "8e492522f9edb4315f1b",
+    "revision": "ce713b19cb53f537691b",
     "url": "/Sheeshal/static/css/main.41de52cd.chunk.css"
   },
   {
-    "revision": "d9e7117d464babe417f2",
-    "url": "/Sheeshal/static/js/2.8c0308e9.chunk.js"
+    "revision": "68f07127826cf569e326",
+    "url": "/Sheeshal/static/js/2.16901b6b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Sheeshal/static/js/2.8c0308e9.chunk.js.LICENSE.txt"
+    "url": "/Sheeshal/static/js/2.16901b6b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8e492522f9edb4315f1b",
-    "url": "/Sheeshal/static/js/main.975a8ad0.chunk.js"
+    "revision": "ce713b19cb53f537691b",
+    "url": "/Sheeshal/static/js/main.6adc3eb9.chunk.js"
   },
   {
     "revision": "57ecd6bfafb582fdf733",
